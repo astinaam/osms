3a7de25a91daf67a48e032a874fb84b630aa47f9
astinaam <abdulahalmahmud@gmail.com>
2017-10-23 19:06:53 +0600

    Added css on error file

--- app/view/view.error.php
+++ app/view/view.error.php
@@ -2,11 +2,18 @@
 
 <head>
     <title>404 Not Found</title>
+    <link rel="stylesheet" href="http://localhost/osms/bootstrap/css/bootstrap.min.css">
 </head>
 
-<body style="background-color: #2b542c;">
-<div >
-    <h1 style="font-size: 46px !important; position: fixed; top:150px; left: 500px; color: #eee;;"><p color="darkred">404</p>Not Found</h1>
+<body style="background-color: #666;">
+<div style="position: fixed; top:150px; left: 500px;">
+    <p style="font-size: 46px !important;  color: #eee;;">
+    <span style="color:darkred;">404</span>
+        Not Found
+    </p>
+    <blockquote style="width: 400px; text-align: justify; font-size: 12px;">Please check the url. Either you have error on the url or you don't have the permission to view this page.
+        For any query please contact with us. Thanks.</blockquote>
+    <button style=" margin-top: 10px;" type="button" class="btn btn-success">Go to Home</button>
 </div>
 </body>
 </html>
