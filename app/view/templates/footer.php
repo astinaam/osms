fc1ddb1ea43d511cebd41f26fe1e4d38c12e6338
astinaam <abdulahalmahmud@gmail.com>
2017-10-28 03:17:38 +0600

    Update footer and design

--- app/view/templates/footer.php
+++ app/view/templates/footer.php
@@ -1,6 +1,10 @@
 <!--<footer>Copyright @Unitech</footer>-->
-<footer style="height:200px; background: linear-gradient(-315deg,#00C04A,darkolivegreen); margin-top: 50px;">
-
+<footer style="height:50px; background: linear-gradient(-315deg,#00C04A,darkolivegreen); margin-top: 50px; color: white; font-weight: bold;">
+    <div class="text-center " style="padding-top: 15px;">
+        <p>
+            Copyright &copy; Unitech Products Limited, Gollamari, Khulna
+        </p>
+    </div>
 </footer>
 </body>
 </html>
