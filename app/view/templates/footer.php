1d321c17d26f00efba5a8c39a13c50c662a3cd00
astinaam <abdulahalmahmud@gmail.com>
2017-10-28 04:21:55 +0600

    Fixed Footer

--- app/view/templates/footer.php
+++ app/view/templates/footer.php
@@ -1,5 +1,6 @@
 <!--<footer>Copyright @Unitech</footer>-->
-<footer style="height:50px; background: linear-gradient(-315deg,#00C04A,darkolivegreen); margin-top: 50px; color: white; font-weight: bold;">
+<footer style="position: relative; bottom: 0px; width: 100%; height:50px;
+background: linear-gradient(-315deg,#00C04A,darkolivegreen); margin-top: 50px; color: white; font-weight: bold;">
     <div class="text-center " style="padding-top: 15px;">
         <p>
             Copyright &copy; Unitech Products Limited, Gollamari, Khulna
