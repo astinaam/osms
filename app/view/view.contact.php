bb77238c0328358ddb6dba41497932c53f5629f6
astinaam <abdulahalmahmud@gmail.com>
2017-10-27 22:34:03 +0600

    Added Map to contact page

--- app/view/view.contact.php
+++ app/view/view.contact.php
@@ -14,7 +14,7 @@
             </p>
         </div>
     </div>
-    <div class="row center-block">
-
+    <div class="row center-block" style="width:800px;">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3678.048292636923!2d89.53798661448398!3d22.80067438506588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff8551cac91257%3A0xe0bb1ddc09119267!2sGallamari+Bus+Stop!5e0!3m2!1sen!2sbd!4v1509121917084" width="800" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
 </div>
